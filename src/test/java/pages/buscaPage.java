9febc714301ffe3fa77d979db2648827a110fef7
LuizFmjr <lzfmartinsjr@gmail.com>
2025-03-06 21:12:55 -0300

    first commit

--- src/test/java/pages/buscaPage.java
+++ src/test/java/pages/buscaPage.java
@@ -10,45 +10,47 @@ import suporte.screenshot;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
-
-
-
 
 public class buscaPage {
 
-		public static WebDriver navegador;
-		String pesquisa = "pesquisa";
-		
-	    @Rule
-	    public static TestName test = new TestName();
-	    
-	    @Test
-	    public void que_acesso_o_blog_Soc() throws Throwable {
-	    	//navegador = Web.createChrome();
-	        	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Luiz Fernando\\Documents\\Qaut\\java\\sitesoc\\drives\\chromedriver.exe");
-	            navegador = new ChromeDriver();
-	        	navegador.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-	        //Navegando para pagina
-	        	navegador.get("https://www.soc.com.br/blog-de-sst/");
-	        	navegador.manage().window().maximize();
-	    }
+	public static WebDriver navegador;
+	String pesquisa = "pesquisa";
 
-	    @Test
-	    public void preencho_o_campo_de(String pesquisa) throws Throwable {
-	        // Write code here that turns the phrase above into concrete actions
-	    	navegador.findElement(By.xpath("//div[2]/div/div/section/div/div/div/div/div/div/div/form/div/input")).click();
-	    	navegador.findElement(By.xpath("//div[2]/div/div/section/div/div/div/div/div/div/div/form/div/input")).sendKeys(pesquisa);
-	    	navegador.findElement(By.xpath("//button/div")).click();
-	    }
+	@Rule
+	public static TestName test = new TestName();
 
-	    @Test
-	    public void visualizo_o_resultado_da_pesquisa() throws Throwable {
-	    	WebElement mensagemPop = navegador.findElement(By.tagName("span"));
-	        String msg = mensagemPop.getText();
-	        assertEquals("span", "span");
-			screenshot.tirar(navegador, "C:\\Users\\Luiz Fernando\\Documents\\Qaut\\java\\sitesoc\\evidencia\\" + generator.dataHoraParaArq() + "Resultado.png");
-	        navegador.close();
-	    }
+	@Before
+	public void setUp() throws Throwable {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Luiz Fernando\\Documents\\Qaut\\java\\sitesoc\\drives\\chromedriver.exe");
+		navegador = new ChromeDriver();
+		navegador.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		navegador.get("https://www.soc.com.br/blog-de-sst/");
+		navegador.manage().window().maximize();
+	}
 
+	@Test
+	public void que_acesso_o_blog_Soc() throws Throwable {
+		// O navegador já foi configurado no método setUp
+	}
+
+	@Test
+	public void preencho_o_campo_de(String pesquisa) throws Throwable {
+		navegador.findElement(By.xpath("//div[2]/div/div/section/div/div/div/div/div/div/div/form/div/input")).click();
+		navegador.findElement(By.xpath("//div[2]/div/div/section/div/div/div/div/div/div/div/form/div/input"))
+				.sendKeys(pesquisa);
+		navegador.findElement(By.xpath("//button/div")).click();
+	}
+
+	@Test
+	public void visualizo_o_resultado_da_pesquisa() throws Throwable {
+		WebElement mensagemPop = navegador.findElement(By.tagName("span"));
+		String msg = mensagemPop.getText();
+		assertEquals("span", "span");
+		screenshot.tirar(navegador, "C:\\Users\\Luiz Fernando\\Documents\\Qaut\\java\\sitesoc\\evidencia\\"
+				+ generator.dataHoraParaArq() + "Resultado.png");
+		navegador.close();
+	}
 }
